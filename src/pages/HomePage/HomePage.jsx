f429a7cb5c29b3b491bcb731145756b4c46ce2a7
Olivia Burgess <burgess.olivia64@gmail.com>
2024-09-24 10:43:54 +0100

    camera file deleted, all in home page.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,26 @@
 import "./HomePage.scss";
-import React, { useState } from "react";
-import Camera from "../../components/Camera/Camera";
+import React, { useState, useRef } from "react";
+// import Camera from "../../components/Camera/Camera";
 import { IoCameraOutline } from "react-icons/io5";
 import axios from "axios";
+import { TbCapture } from "react-icons/tb";
+import Webcam from "react-webcam";
 
-const apiUrl = import.meta.env.VITE_API_URL; // Access the API URL from .env
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export default function HomePage() {
   const [showCamera, setShowCamera] = useState(false);
   const [image, setImage] = useState(null);
+  const webRef = useRef(null);
+
+  const showImage = () => {
+    if (webRef.current) {
+      const screenshot = webRef.current.getScreenshot();
+      setImage(screenshot);
+      setShowCamera(false);
+      console.log(screenshot);
+    }
+  };
 
   const handleClick = (event) => {
     event.preventDefault();
@@ -16,19 +28,40 @@ export default function HomePage() {
     setImage(null);
   };
 
+  const handleSubmitClick = async () => {
+    if (image) {
+      const response = await axios.post(`${apiUrl}/upload`, {
+        imageData: image,
+      });
+      console.log(response.data);
+    }
+  };
+
   return (
     <section className="main">
       <h1 className="main__title"> Tap to find out </h1>
-      {/* <button onClick={handleClick} className="main__button">
-        show camera
-      </button> */}
-      <div className="main__img-div">
+      <section className="cam">
         {showCamera && (
-          <Camera setImage={setImage} setShowCamera={setShowCamera} />
+          <Webcam ref={webRef} alt="webcam screenshot" height={540} />
         )}
-        <IoCameraOutline onClick={handleClick} className="main__img" />
-      </div>
-      <img src={image} />
+        {!showCamera && !image && (
+          <IoCameraOutline onClick={handleClick} className="main__img" />
+        )}
+        <img src={image} />
+        {showCamera && (
+          <TbCapture
+            onClick={() => {
+              showImage();
+            }}
+          />
+        )}
+        {image && (
+          <button className="cam__submit" onClick={handleSubmitClick}>
+            Submit
+          </button>
+        )}
+        {/* </div> */}
+      </section>
     </section>
   );
 }
