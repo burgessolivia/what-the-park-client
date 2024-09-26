153f950db1f2823ebbc5d67690dc38b1dfe4ed5d
Olivia Burgess <burgess.olivia64@gmail.com>
2024-09-26 23:04:51 +0100

    about section done for different device sizing

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,22 @@
 import "./AboutPage.scss";
 
-export default function () {
+export default function AboutPage() {
   return (
     <section className="about">
-      <h2>this is the about page </h2>
+      <div className="about__section">
+        <div className="about__section-text">
+          <h2 className="about__section-title"> What The Park explained...</h2>
+          <p className="about__section-p">
+            As a driver I find it frustrating when you need to park and get on
+            with your day, but you're presented with a confusing parking sign.
+            Thats where the idea for What The Park came from. What The Park
+            allows the user to take a picture of said confusing parking sign on
+            the web app and then simplifies the parking sign. <br /> <br /> So
+            there's no need anymore to wait around wasting time while decodes
+            works out the sign, because WTP can do it for you!
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -128,7 +128,6 @@ export default function HomePage() {
             />
           </Link>
         )}
-        ;
       </section>
     </section>
   );
