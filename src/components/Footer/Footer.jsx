a585bf35b81c094599fee9585743f49141fecf88
Olivia Burgess <burgess.olivia64@gmail.com>
2024-09-26 16:53:55 +0100

    body background colour fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,20 @@
 import "./Footer.scss";
-import { Link } from "react-router-dom";
 import gitHub from "../../assets/icons/github.svg";
 import linkedIn from "../../assets/icons/linkedin.svg";
 
 export default function Footer() {
   return (
     <section className="footer">
-      <Link href="github.com/burgessolivia">
+      <a href="https://github.com/burgessolivia">
         <img src={gitHub} alt="github icon" className="footer__img-git" />
-      </Link>
-      <Link href="linkedin.com/in/oliveburgess">
+      </a>
+      <a href="https://linkedin.com/in/oliveburgess">
         <img
           src={linkedIn}
           alt="linkedIn icon"
           className="footer__img-linked"
         />
-      </Link>
+      </a>
     </section>
   );
 }
